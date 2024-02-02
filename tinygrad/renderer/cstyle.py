a3afb39a566c59404ba06e17c519fb54a88a8e7a
peters9981 <86070675+peters9981@users.noreply.github.com>
2024-02-01 20:32:53 -0600

    update

--- tinygrad/renderer/cstyle.py
+++ tinygrad/renderer/cstyle.py
@@ -102,7 +102,7 @@ def uops_to_cstyle(lang:CStyleLanguage, function_name:str, uops:List[UOp]) -> st
   print('lang', lang)
   print('function_name', function_name)
   print('uops', uops)
-  if lang isinstance(CUDALanguage):
+  if isinstance(lang, CUDALanguage):
     print('Put CCode here')
   local_size: List[int] = []
   kernel,prekernel,bufs = [],[],[]
