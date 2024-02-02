fba261378ad438d59bb675d8f419841cf804d0f5
peters9981 <86070675+peters9981@users.noreply.github.com>
2024-02-01 22:22:22 -0600

    update

--- tinygrad/runtime/ops_cuda.py
+++ tinygrad/runtime/ops_cuda.py
@@ -46,7 +46,7 @@ class CUDAProgram:
     if not CUDACPU:
       check(cuda.cuCtxSetCurrent(self.device.context))
       self.module = init_c_var(cuda.CUmodule(), lambda x: check(cuda.cuModuleLoadData(ctypes.byref(x), lib)))
-      check(cuda.cuModuleGetFunction(ctypes.byref(prg := cuda.CUfunction()), self.module, name.encode("utf-8")))
+      #check(cuda.cuModuleGetFunction(ctypes.byref(prg := cuda.CUfunction()), self.module, name.encode("utf-8")))
     self.prg = prg if not CUDACPU else lib
 
   def __del__(self):
